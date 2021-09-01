f2c8a15efca617743dc48256529286b3453323b5
puckvis <puck@puckvisser.nl>
2021-09-01 11:26:47 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const message =
+  "Hi there, classmate! Starting this bootcamp was already a big achievement. Coding for three days in a row now. You're doing really great! And there's a lot more to come. I believe you can do this :-) Keep up the good work!";
+
+console.log(message);
